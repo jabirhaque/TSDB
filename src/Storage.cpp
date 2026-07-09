57aa0267fee6ffc42c4c6ac5f8ab8c112f534a27
Jabir Nurul Haque <jabirhaque@hotmail.co.uk>
2026-07-09 12:08:17 +0100

    introducing read write lock

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -72,6 +72,7 @@ bool Storage::append(Record r)
 }
 
 std::vector<Record> Storage::readAll() const {
+    std::shared_lock lock(readWriteMutex);
     std::ifstream inFile(filename, std::ios::binary);
     if (!inFile.is_open()) {
         throw std::runtime_error("Failed to open file for reading: " + filename);
@@ -112,6 +113,7 @@ std::vector<Record> Storage::readAll() const {
 
 std::vector<Record> Storage::readRange(int64_t startTs, int64_t endTs) const
 {
+    std::shared_lock lock(readWriteMutex);
     if (startTs > endTs) throw std::runtime_error("Invalid time range");
 
     if (startTs > lastTimestamp) return {};
@@ -211,6 +213,7 @@ std::optional<Record> Storage::readFromTime(int64_t timestamp) const
 
 std::optional<Record> Storage::getLastRecord() const
 {
+    std::shared_lock lock(readWriteMutex);
     std::ifstream inFile(filename, std::ios::binary);
     if (!inFile.is_open()) throw std::runtime_error("Failed to open file: " + filename);
 
@@ -237,6 +240,7 @@ std::optional<Record> Storage::getLastRecord() const
 
 Record Storage::getRecord(size_t index) const
 {
+    std::shared_lock lock(readWriteMutex);
     std::ifstream inFile(filename, std::ios::binary);
     if (!inFile.is_open()) throw std::runtime_error("Failed to open file: " + filename);
 
@@ -261,6 +265,7 @@ Record Storage::getRecord(size_t index) const
 
 int64_t Storage::getLastTimestamp() const
 {
+    std::lock_guard<std::mutex> lock(bufferMutex);
     return lastTimestamp;
 }
 
@@ -271,6 +276,7 @@ TSDBHeader Storage::getHeader() const
 
 size_t Storage::getRecordCount() const
 {
+    std::shared_lock lock(readWriteMutex);
     return recordCount;
 }
 
@@ -279,8 +285,9 @@ size_t Storage::getSparseIndexStep() const
     return sparseIndexStep;
 }
 
-const std::vector<IndexEntry>& Storage::getSparseIndex() const
+std::vector<IndexEntry> Storage::getSparseIndex() const
 {
+    std::shared_lock lock(readWriteMutex);
     return sparseIndex;
 }
 
@@ -401,6 +408,7 @@ void Storage::flushLoop()
 }
 
 void Storage::flushBufferToDisk(std::vector<Record>& batch) {
+    std::unique_lock lock(readWriteMutex);
 
     size_t bytes = batch.size() * sizeof(Record);
 
